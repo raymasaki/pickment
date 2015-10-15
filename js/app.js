7d5d20bfa20771a617a30ea0d70a2217df2b193e
Ray Masaki <ray@raymasaki.com>
2015-10-15 10:20:45 -0400

    finished intro logic

--- js/app.js
+++ js/app.js
@@ -10,10 +10,10 @@
 console.log('Loaded');
 
 /*
-1. User clicks start
-2. hides intro div
-3. reveals level count div
-4. level count div hides after 1 seconds
+## 1. User clicks start
+## 2. hides intro div
+## 3. reveals level count div
+## 4. level count div hides after 1 second
 5. level count var is set in header and timer begins countdown
 6. game shows colorWord from an array with random colorVal from array
 7. mouse has corresponding colorVal = colorWord
@@ -22,20 +22,53 @@ console.log('Loaded');
 10. if incorrect reveals intro div with updated highscore and start replaced with play again
 */
 
-/*
-Logic for creating cells:
-- Math.round(square root of level) = how many rows
-- level/sqrt = how many per row
-- 100 / cells per row = % width of each cell
-- level % sqrt = how many cells on the last row
-- create a colorWord element in the cell
-*/
+/* Intro Logic */
 
-/*
-colorWord logic:
-- number of colorWord is equal to level number
-- currentColors is an array of colorVals
-- display each colorWord with a noncorresponding colorVal
-- assign mouseColor with one of the colorVals
-- mouseColor clicks colorWord proceed to next level
-*/
+$(document).ready(function() {
+
+  var $startButton = $('.start_button');
+  var $intro = $('#intro');
+
+  // Clicking start button will fade out intro and show level count intro
+  $startButton.click(function(e) {
+    $intro.fadeOut('fast');
+    levelCountIntro();
+  });
+
+
+
+  /* Level Count */
+
+  var levelCount = 0;
+
+  var $levelCountIntro = $('#level-count > h1');
+  $levelCountIntro.text("Level " + levelCount);
+
+  // level count intro fades out after 1 second
+  var levelCountIntro = function () {
+    setTimeout( function() {
+      $('#level-count').fadeOut('fast');
+    }, 1000);
+  };
+
+
+  /*
+  Logic for creating cells:
+  - Math.round(square root of level) = how many rows
+  - level/sqrt = how many per row
+  - 100 / cells per row = % width of each cell
+  - level % sqrt = how many cells on the last row
+  - create a colorWord element in the cell
+  */
+
+  /*
+  colorWord logic:
+  - number of colorWord is equal to level number
+  - currentColors is an array of colorVals
+  - display each colorWord with a noncorresponding colorVal
+  - assign mouseColor with one of the colorVals
+  - mouseColor clicks colorWord proceed to next level
+  */
+
+
+});
